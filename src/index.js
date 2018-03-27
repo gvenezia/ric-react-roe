eded87cd5f63208c5103367064377ddbc11c3368
gvenezia <g.venezia@outlook.com>
2018-03-27 22:14:45 +0000

    Add alternating turns b/w X and O
    Change the state in Board to allow for alternating turns.
    
    Add a state value to the constructor, handleClick event, and the status display on render().

--- src/index.js
+++ src/index.js
@@ -18,13 +18,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   
   renderSquare(i) {
@@ -37,7 +41,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status =  this.state.xIsNext ? 'Next player: X' : 'Next player: O';
 
     return (
       <div>
