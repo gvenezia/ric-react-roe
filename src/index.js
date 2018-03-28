0c52857af9a030ab53d54f834920085aa6bf24c3
gvenezia <g.venezia@outlook.com>
2018-03-28 23:57:42 +0000

    Enable turn-taking and winning the game
    Add conditional logic to enable turn-taking and correctly display who's turn is next, 'X', or 'O'.
    
    Add a helper function to calculate whether the most recent turn results in a win. Use this function to stop the game from continuing after a win.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// ============= Components ==================
 function Square(props){
     return (
       <button className="square" onClick={props.onClick} >
@@ -24,6 +25,11 @@ class Board extends React.Component {
   
   handleClick(i){
     const squares = this.state.squares.slice();
+    
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
+    
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares,
@@ -41,7 +47,17 @@ class Board extends React.Component {
   }
 
   render() {
-    const status =  this.state.xIsNext ? 'Next player: X' : 'Next player: O';
+    // const works here, because each time the Board is rerendered, there is a new value for the status and so 
+    // there is a new const being declared (is the old one garbage collected?)
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    if (winner){
+      status = `Winner: ${winner}`;
+    } else {
+      status = this.state.xIsNext ? `Next player: X` : `Next player: O`;
+    }
+    
 
     return (
       <div>
@@ -82,7 +98,30 @@ class Game extends React.Component {
   }
 }
 
-// ========================================
+// ================= Functions ==================
+// Helper function
+function calculateWinner(squares) {
+  // hard coded winning combinations
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// =============== React DOM Render =========================
 
 ReactDOM.render(
   <Game />,
